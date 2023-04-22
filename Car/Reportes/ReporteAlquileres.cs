b49d88750c09a756334a9f5071ba59476a4f0aed
Danir1509 <2524572019@mail.utec.edu.sv>
2023-04-22 01:22:20 -0600

    cambio 01:21

---
+++ Car/Reportes/ReporteAlquileres.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car.Reportes
+{
+    public partial class ReporteAlquileres : Form
+    {
+        public ReporteAlquileres()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteAlquileres_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
